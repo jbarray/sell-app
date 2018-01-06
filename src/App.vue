bc227ace696eb193de1255dea1894b7450d9ffcb
jby <1614963384@qq.com>
2018-01-06 12:13:55 +0800

    测试1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
-//<link rel="stylesheet" href="common/stylus/mixin.styl">
+<!--//<link rel="stylesheet" href="common/stylus/mixin.styl">-->
 <template xmlns="http://www.w3.org/1999/html">
-  <div id="content">测试
-    <!--<div class="tab">-->
-    <!--<div class="tab-item">-->
-    <!--<router-link to="/goods">商品</router-link>-->
-    <!--</div>-->
-    <!--<div class="tab-item">-->
-    <!--<router-link to="/goods">评论</router-link>-->
-    <!--</div>-->
-    <!--<div class="tab-item">-->
-    <!--<router-link to="/goods">商家</router-link>-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--<router-view></router-view>-->
+  <div id="content">
+    <div class="tab border-1px">
+    <div class="tab-item">
+    <router-link to="/goods">商品</router-link>
+    </div>
+    <div class="tab-item">
+    <router-link to="/goods">评论</router-link>
+    </div>
+    <div class="tab-item">
+    <router-link to="/goods">商家</router-link>
+    </div>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 <script>
@@ -20,14 +20,20 @@
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
   //引入1像素文件
-  /*@import "./common/stylus/mixin.styl"*/
+  @import "./common/stylus/mixin.styl"
  .tab
-  display flex
-  width:100%
-  height: 40px
-  line-height 40px
-
-  .tab-item
-    flex: 1
-    line-height:40px
+   display flex
+   width:100%
+   height: 40px
+   line-height 40px
+   border-bottom:1px solid rgba(7,17,27,0.1)
+   .tab-item
+     flex: 1
+     text-align center
+     & > a
+       display:block
+       font-size: 14px
+       color:rgb(77,85,93)
+       &:active
+         color:rgb(240,20,20)
 </style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // const Vue =require('vue')
-import Vue from 'vue/dist/vue.common'
+//import Vue from 'vue/dist/vue.common'
 //const VueRouter=require('vue-router')
- //import Vue from 'vue'
+ import Vue from 'vue'
  import App from '../src/App.vue'
-// import VueRouter from 'vue-router'
-// import goods from './components/goods/goods.vue'
-// const routes=[
-//   {path: '/goods', component: goods}
-// ]
-// Vue.use(VueRouter)
-// const router = new VueRouter({
-//   routes
-// })
+ import VueRouter from 'vue-router'
+ // import VueResource from 'vue-resource'
+ import goods from './components/goods/goods.vue'
+ import './common/stylus/index.styl'
+ const routes=[
+  {path: '/goods', component: goods}
+]
+Vue.use(VueRouter)
+// Vue.use(VueResource)
+const router = new VueRouter({
+  routes
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#content',
-  render:(createElement)=>createElement(App),
+ // render:(createElement)=>createElement(App),
   // template: '<App/>',
-  // components: { App }
-  //router
+   components: { App },
+  router
 })
